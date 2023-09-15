b133e428b60caf6b8451823b8b962b43edba54c0
Dol arnold <zedekiadol@gmail.com>
2023-09-15 12:16:43 +0300

    first commit

---
+++ admin/src/App.js
@@ -0,0 +1,15 @@
+import './assets/css/styles.css'
+import Nav from './components/partials/Nav';
+import SideBar from './components/partials/SideBar';
+function App() {
+  return (
+    <div>
+    <Nav/>
+    <div id="LayoutSidenav"></div>
+    <SideBar/>
+    </div>
+  
+  );
+}
+
+export default App;
